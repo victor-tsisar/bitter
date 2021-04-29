c96f0adfb14ce7c9e1000c7d4e6a350f37959ace
Victor Tsisar <tsisar.victor@gmail.com>
2021-04-29 23:25:58 +0300

    mobile adaptive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     "use strict";
 
+    const width = $(document).width();
     const tab = $(".reviews__tabs-name");
     const readMore = $(".reviews__link");
+    let visibleReviews = $(".reviews__tile.active").height();
+    let moreReviews = visibleReviews;  //  start point
     const btn = $(".btn[type='button']");
     const btnForm = $(".btn[type='submit']");
     const name = $("#name");
@@ -14,6 +17,14 @@ $(document).ready(function () {
         "class": 'overlay',
     }).appendTo('body');
     const overlay = $('.overlay');
+
+    if (width <= 768) {
+        $('.header__form').appendTo($('.header__promo'));
+    }
+    
+    if (width <= 1440) {
+        $('.history__contact').appendTo($('.history__promo'));
+    }
 
     function openAlert() {
         alertWindow.addClass("active");
@@ -29,13 +40,13 @@ $(document).ready(function () {
 
     btn.on('click', function (event) {
         event.preventDefault();
-        openAlert();
+        setTimeout(openAlert, 100);
     });
 
     btnForm.on('click', function (event) {
-        if(name.val() && phone.val() || mail.val()) {
+        if (name.val() && phone.val() || mail.val()) {
             event.preventDefault();
-            openAlert();
+            setTimeout(openAlert, 100);
         } else {
             event.preventDefault();
             alert('Введите верные значения!')
@@ -43,12 +54,20 @@ $(document).ready(function () {
     });
 
     statusBtn.on('click', closeAlert);
-    
+
     overlay.on('click', closeAlert);
-    
+
     readMore.on('click', function () {
-        $(".reviews__tile").css('max-height', 'unset');
-        readMore.css('opacity', '0.4');
+        moreReviews += visibleReviews;        
+        $(".reviews__tile").css('max-height', moreReviews);
+
+        let heightReviews = parseInt($(".reviews__tile.active").css('height'));
+
+        if (moreReviews > heightReviews) {
+            readMore.css('display', 'none');
+            $(".reviews__tile.active").css('background', '#f6f9f3');
+            moreReviews = visibleReviews;
+        }
     });
 
     tab.on('click', function () {
@@ -56,9 +75,11 @@ $(document).ready(function () {
         $(this).toggleClass("current");
         $(".reviews__tile").toggleClass("active");
         $(".reviews__tile").css('max-height', '470px');
-        readMore.css('opacity', '1');
+        $(".reviews__tile.active").css('background',
+            'linear-gradient(to top, #c5c5c5, #f6f9f3 15%, #f6f9f3 100%)');
+        readMore.css('display', 'block');
     });
-    
+
     $('.slider').slick({
         infinite: true,
         arrows: false,
@@ -67,6 +88,43 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: '100px',
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 470,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 
 });
